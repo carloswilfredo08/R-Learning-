70f4c141e1aaf04739bd6b9d8b3dd7618c257b82
Willy CA <carloswilfredo08@gmail.com>
2021-01-17 21:49:31 +0100

    Carlos Contreras Assignment

---
+++ Assignment Lexical scoping .R
@@ -0,0 +1,29 @@
+## Lexical Scoping ----
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
+
+## Caching the inverse of matrix ----
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+}
+
